662eb67dd1d8d10e0058322ca69d33721e9c107d
awais144506 <awaisrasheed68@gmail.com>
2024-10-26 13:46:00 +0500

    Done

--- frontend/components/Cereals.js
+++ frontend/components/Cereals.js
@@ -1,12 +1,37 @@
-import React, { useState } from 'react'
-
+import React, { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
 export default function Cereals() {
   const [cereals, setCereals] = useState([])
-
+  const navigate = useNavigate()
   const logout = () => {
-
+    localStorage.removeItem("token")
+    navigate("/")
   }
-
+  useEffect(() => {
+    const token = localStorage.getItem("token")
+    if(!token)
+    {
+      navigate("/")
+    }
+    else
+    {
+      try{
+        const fetchCereals =async () =>{
+            const response = await axios.get(
+              "/api/cereals",
+              {headers:{Authorization:token}}
+            )
+            setCereals(response.data)
+        }
+        fetchCereals()
+      }
+      catch(error)
+      {
+        if(error?.response?.data?.message ==401) logout()
+      }
+    }
+  }, [])
   return (
     <div className="container">
       <h3>Cereals List <button onClick={logout}>Logout</button></h3>
